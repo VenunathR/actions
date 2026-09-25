ed54b3990075b98496f4fc1fcd6fa4986950045c
VenunathR <venunath756@gmail.com>
2026-09-25 10:11:55 +0530

    Initial GitHub Actions demo

---
+++ app.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("GitHub Actions Demo")
+    print("2 + 3 =", add(2, 3))
+    print("2 × 3 =", multiply(2, 3))
